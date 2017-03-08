048ac02ee703acff73d7c7a09081d4fdd159ab6f
luisresende <luisfrvilela@gmail.com>
2017-03-08 10:50:40 -0300

    update content and css community-relationship-container

--- app/components/community/community-relationship-container.js
+++ app/components/community/community-relationship-container.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     contentMainObject:{
         title:"RELACIONAMENTO É A NOSSA VOCAÇÃO",
-        description:"Ao explorar todos os benefícios da nossa rede, você pode encontrar parceiros e clientes para o seu negócio, compartilhar e adquirir novas experiências e ficar por dentro dos melhores eventos de empreendedorismo da região. Se identificou e quer fazer parte? Veja abaixo suas opções:"
+        description:"Ao explorar todos os benefícios da nossa rede, você pode encontrar parceiros e clientes para o seu negócio, compartilhar e adquirir novas experiências e ficar por dentro dos melhores eventos de empreendedorismo da região. Se identificou e quer fazer parte? Além de ser membro, veja outras opções para participar do nosso time:"
     }
 });
