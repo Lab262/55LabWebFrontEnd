3987efbee5de102af4437c20834690d4519c75a7
luisresende <luisfrvilela@gmail.com>
2017-03-08 10:46:54 -0300

    update content and css community-benefits-container

--- app/components/community/community-advantage-container.js
+++ app/components/community/community-advantage-container.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
 
    contentObject: {
     image:"/assets/images/white-logo-55-community.png",
-    descriptionFirst: "Nossa comunidade se conecta por meio de uma plataforma digital que possibilita a troca e o desenvolvimento de ideias entre empreendedores locais. Quem é membro tem benefícios exclusivos: descontos em produtos e serviços selecionados, acesso a programas e cursos com conteúdos relevantes e eventos para networking. Ser membro de nossa comunidade também garante, por mês, sem custo adicional, 1 hora para ser usada em sala de reunião - meeting room - ou na estações de trabalho - open desk e, se precisar de mais tempo o desconto é garantido.",
-    descriptionSecond: "Esse é um plano perfeito para quem quer começar uma conexão com a comunidade empreendedora, mas só precisa do espaço de trabalho poucos dias no mês. "
+    descriptionFirst: "Nossa comunidade de conecta por meio de uma plataforma digital que possibilita a troca e o desenvolvimento de ideias entre empreendedores. Nossos membros têm benefícios exclusivos: descontos em produtos e serviços selecionados, acesso a programas e cursos com conteúdos relevantes para a gestão de seus negócios e eventos para networking. Ser membro de nossa comunidade também garante, sem custo adicional, 1 hora de sala de reunião (meeting room) ou de estação de trabalho (open desk) por mês.",
+    descriptionSecond: "Esse é um plano perfeito para quem quer começar uma conexão com nossa comunidade empreendedora, mas só precisa de espaço de trabalho poucas horas no mês."
   }
 });
--- app/components/community/community-benefits-container.js
+++ app/components/community/community-benefits-container.js
@@ -4,32 +4,32 @@ export default Ember.Component.extend({
     contentMainObject:{
         title:"MOTIVOS DE SOBRA PARA SE CONECTAR",
         firstButton:"SEJA MEMBRO",
-        secondButton:"CONHEÇA NOSSOS PLANOS"
+        secondButton:"CONHEÇA NOSSOS ESPAÇOS"
     },
     
     contentObjectLeftUp: {
         title: "NETWORKING",
-        description: "Conexão on e offline com uma base que já começa com mais de 400 empreendedores, mentores, colaboradores e investidores para compartilhar ideias, experiências e oportunidades de trabalho.",
+        description: "Conexão on e offline com uma base que já começa com mais de 400 empreendedores, mentores, colaboradores e investidores que compartilham ideias, experiências e oportunidades de trabalho.",
         image:"/assets/images/icon_network.png"
     },
 
     contentObjectRightUp: {
-        title: "ACESSO AO ESPAÇO",
-        descriptionOne: "Acesso flexível ao espaço: como membro, você terá 01 hora em ",
+        title: "ACESSO AOS ESPAÇOS +55LAB",
+        descriptionOne: "Acesso flexível aos espaços +55Lab.co: como membro, você terá 01 hora em ",
         linkOne:"meeting room ou open desk",
-        descriptionTwo: " por mês e ficará por dentro de tudo que rola nos espaços de trabalho com exclusividade.",
+        descriptionTwo: " por mês e ficará por dentro de tudo o que rola nos espaços de trabalho com exclusividade.",
         image:"/assets/images/icon_space.png"
     },
 
     contentObjectLeftDown: {
         title: "EVENTOS EXCLUSIVOS",
-        description: "A +55Lab.co promove uma série de eventos sociais, palestras, programas de captação de workshops e cursos nos quais você pode adquirir conhecimento e compartilhar ideias sobre o universo empreendedor.",
+        description: "A +55Lab.co promove uma série de eventos sociais, palestras, programas de capacitação, workshops e cursos nos quais você pode adquirir conhecimento e compartilhar ideias sobre o universo empreendedor.",
         image:"/assets/images/icon_events.png"
     },
 
     contentObjectRightDown: {
         title: "DESCONTOS ESPECIAIS",
-        descriptionOne: "Quem é membro tem a sua disposição uma rede de descontos em serviços que ajudam o empreendedor a cuidar melhor de si, da sua empresa e de seus colaboradores. Serviços jurídicos e de comunicação a um preço mais em conta e descontos em planos de saúde, seguros e ferramentas são alguns benefícios que estão disponíveis. Acompanhe no ",
+        descriptionOne: "Nossos membros tem acesso a uma rede de descontos em produtos e serviços que ajudam o empreendedor a cuidar melhor de si, da sua empresa e de seus colaboradores. Planos de saúde, os mais diversos tipos de seguros e ferramentas de comunicação digital são alguns dos benefícios que estão disponíveis. Acompanhe no ",
         linkOne:"blog",
         descriptionTwo: " as novidades.",
         image:"/assets/images/icon_discounts.png"
