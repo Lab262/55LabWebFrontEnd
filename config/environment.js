5a3a1efba4a182efa542264f59269420463efa4d
thiagotmb <tmb0710@gmail.com>
2017-07-11 02:29:22 -0400

    Fixing GTM

--- config/environment.js
+++ config/environment.js
@@ -61,30 +61,24 @@ module.exports = function(environment) {
 
   };
 
+    ENV['ember-cli-gtm'] = {
+      appId: 'GTM-P62ZBH4'
+  };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
     ENV.baseURL = null;
     ENV.locationType = 'hash';
-
   }
 
   return ENV;
